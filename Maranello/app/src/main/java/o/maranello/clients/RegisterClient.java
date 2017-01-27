0abf38cf13320ce78352e31ef686b75489974bdc
Kris Thornley <thornleyk@gmail.com>
2017-01-27 19:52:50 +1300

    Add REST Clients

---
+++ Maranello/app/src/main/java/o/maranello/clients/RegisterClient.java
@@ -0,0 +1,22 @@
+package o.maranello.clients;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by kristianthornley on 27/01/17.
+ */
+public class RegisterClient {
+
+    private static final String BASE_URL = "http://maranelloeventprocessor.mybluemix.net/app/register";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void get(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.get(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
---
+++ Maranello/app/src/main/java/o/maranello/clients/WatsonClient.java
@@ -0,0 +1,22 @@
+package o.maranello.clients;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+/**
+ * Created by kristianthornley on 27/01/17.
+ */
+public class WatsonClient {
+
+    private static final String BASE_URL = "http://j4698t.messaging.internetofthings.ibmcloud.com:1883/api/v0002/device/types/Maranello_App/devices/pi0001/events";
+    private static AsyncHttpClient client = new AsyncHttpClient();
+
+    public static void post(String url, RequestParams params, AsyncHttpResponseHandler responseHandler) {
+        client.post(getAbsoluteUrl(url), params, responseHandler);
+    }
+
+    private static String getAbsoluteUrl(String relativeUrl) {
+        return BASE_URL + relativeUrl;
+    }
+}
